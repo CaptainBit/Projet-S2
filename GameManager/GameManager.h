37a6e5f6acc58004fc16d83381c6a480447d7b7c
CaptainBit <fisetisaac@hotmail.com>
2020-02-11 11:50:53 -0500

    Ajout fichier base

---
+++ GameManager/GameManager.h
@@ -0,0 +1,14 @@
+//
+// Created by fiset on 2020-02-11.
+//
+
+#ifndef PROJET_GAMEMANAGER_H
+#define PROJET_GAMEMANAGER_H
+
+
+class GameManager {
+
+};
+
+
+#endif //PROJET_GAMEMANAGER_H
---
+++ Main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by fiset on 2020-02-11.
+//
+
+int main (int argc, char *argv[]) {
+
+}
