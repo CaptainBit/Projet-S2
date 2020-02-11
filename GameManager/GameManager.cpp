3dd2a78c4afdfcb193ef8ce280c9eaf6387f76e6
Unknown <vianneygall@gmail.com>
2020-02-11 17:29:23 -0500

    GameManager.cpp Update

--- GameManager/GameManager.cpp
+++ GameManager/GameManager.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "GameManager.h"
+
+
+GameManager::GameManager()
+{
+	Terrain t(10,10);	// pour un array de 10X10
+	terrain = &t;
+	Joueur un;
+	Joueur deux;
+	Joueur_un = &un;
+	Joueur_deux = &deux;
+}
+GameManager::GameManager(int hauteur, int largeur)
+{
+	Terrain t(hauteur,largeur);	
+	terrain = &t;
+	Joueur un;
+	Joueur deux;
+	Joueur_un = &un;
+	Joueur_deux = &deux;
+}
+GameManager::~GameManager()
+{
+	delete Joueur_un;
+	delete Joueur_deux;
+	delete terrain;
+}
+void GameManager::changer_tour()	//refresh le nombre de pts de deplacement du joueur actif
+{
+	tour = !tour;
+	if(tour){
+		joueur_un->mvt = 3;			//A modifier en fonction de l implementation de la class joueur
+	}
+	else{
+		joueur_deux->mvt = 3;			//A modifier en fonction de l implementation de la class joueur
+	}
+}
+void GameManager::refresh_frame()	//Afficher le UI avec les donnees a jour
+{
+	ui.afficher();
+}
+	
+	
+	//Methodes lies a la gestion d evenements:
+void GameManager::deplacer_joueur()
+{
+	
+}
+void GameManager::tirer()		//Va probablement appeler set_angle et set_puissance de maniere consecutive
+{
+	
+}
+void GameManager::set_angle()
+{
+	
+}
+void GameManager::set_puissance()
+{
+	
+}
--- GameManager/GameManager.h
+++ GameManager/GameManager.h
@@ -5,23 +5,24 @@
 #ifndef PROJET_GAMEMANAGER_H
 #define PROJET_GAMEMANAGER_H
 
-#include UI.h
-#include traitementPhysic.h
-#include traitementProjectile.h
-#include traitementJoueur.h
-#include traitementTerrain.h
+#include "UI.h"
+#include "traitementPhysic.h"
+#include "traitementProjectile.h"
+#include "traitementJoueur.h"
+#include "traitementTerrain.h"
 
 class GameManager {
 
 private:
-	Terrain terrain(int hauteur, int largeur);
+	Terrain *terrain;
 	Joueur *joueur_un;			// Passer en argument les coord de depart?? -- objet provenant de TraitementJour.h
 	Joueur *joueur_deux;		// Passer en argument les coord de depart?? -- objet provenant de TraitementJour.h
 	UI ui;
-	int tour[]= {1,2}
+	bool tour = true;
 	
 public:
 	GameManager();
+	GameManager(int hauteur, int largeur);
 	~GameManager();
 	void changer_tour();	//refresh le nombre de pts de deplacement du joueur actif
 	void refresh_frame();	//Afficher le UI avec les donnees a jour
