bfd297be926e834da02339de33c5f0293854d574
tipat95 <60936214+tipat95@users.noreply.github.com>
2020-02-13 11:05:24 -0500

    Ajout de la classe Joueur
    * Redaction de Joueur.h
    
    * Implémentation de la classe Joueur
    
    implémentation de Joueur.h et de Joueur.cpp
    
    * Update classe Joueur
    
    Ajout de la méthode tirer
    
    * Modif Vecteur
    
    Co-authored-by: felixdupont <47389903+felixdupont@users.noreply.github.com>

--- GameManager/Joueur.cpp
+++ GameManager/Joueur.cpp
@@ -2,4 +2,69 @@
 // Created by fiset on 2020-02-11.
 //
 
+
 #include "Joueur.h"
+Joueur::Joueur()
+{
+	vie = VIE_MAX;
+	pointsDeplacement = POINTS_DEPLACEMENT; 
+}
+int Joueur::getVie()
+{
+	return vie;
+}
+
+Coord Joueur::getPosition()
+{
+	return position;
+}
+
+int Joueur::getTypePrjectile()
+{
+	return typeProjectile;
+}
+
+int Joueur::getPointsDeplacement()
+{
+	return pointsDeplacement
+}
+
+void Joueur::setVie(int vie)
+{
+	this->vie = vie; 
+}
+
+void Joueur::setPosition(Coord position)
+{
+	this->position.x = position.x;
+	this->position.y = position.y;
+}
+
+void Joueur::setTypeProjectile(int type) 
+{
+	typeProjectile = type; 
+}
+
+void Joueur::setPointsDeplacement(int points) 
+{
+	pointsDeplacement = points; 
+}
+
+//Deplacer le tank(Positif vers la droite et negatif vers la gauche). Retourne s'il avait assez de point de deplacement
+bool Joueur::deplacer(int deplacement)
+{
+
+}
+
+//Enleve le nombre de point de degat a la vie. Retourne si le tank est toujours en vie. 
+bool Joueur::endomager(int degat)
+{
+	vie -= degat;
+	return (vie > 0);
+}
+
+Projectile Joueur::tirer(float angle, float force)
+{
+	Projectile projectile(angle, force);
+	return projectile; 
+}
--- Vec2.h
+++ Vec2.h
@@ -4,11 +4,11 @@
 
 #ifndef PROJET_COORD_H
 #define PROJET_COORD_H
-struct Coord{
-    float x;
-    float y;
+struct Vec2{
+    int x;
+    int y;
 
-    Coord(float p_x = 0.0f, float p_y = 0.0f){
+    Coord(int p_x = 0.0f, int p_y = 0.0f){
         x = p_x;
         y = p_y;
     }
