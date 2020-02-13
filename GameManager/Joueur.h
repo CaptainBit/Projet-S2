ef24b86fd01e7b07cbf2374a9a3136672d492922
felixdupont <fxdupont@hotmail.com>
2020-02-12 19:07:13 -0500

    Redaction de Joueur.h

--- GameManager/Joueur.h
+++ GameManager/Joueur.h
@@ -4,9 +4,31 @@
 
 #ifndef PROJET_JOUEUR_H
 #define PROJET_JOUEUR_H
-
-
+#define POINT_DEPLACEMENT=10;
+struct coord {
+	int x;
+	int y;
+};
 class Joueur {
+private:
+	int vie;
+	coord position;
+	int projectileType;
+	int pointsDeplacement;
+public:
+	int getVie();
+	coord getPosition();
+	int getProjectileType();
+	int getPointDeplacement();
+	void setVie(int vie);
+	void setPosition(coord Position);
+	void setProjectileType(int type);
+	void setPointsDeplacement(int points);
+
+	//Deplacer le tank(Positif ver la droit et negatif vers la gauche). Retourne s'il avait assez de point de deplacement
+	bool deplacer(int deplacement);
+	//Enleve le nombre de point de degat a la vie. Retourne si le tank est toujours en vie
+	bool endomager(int degat);
 
 };
 
