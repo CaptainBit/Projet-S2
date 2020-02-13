86168718c42f9b72ce76a6dce08a6492be72dec6
Philippe Lafrance <lafp3202@usherbrooke.ca>
2020-02-13 10:46:51 -0500

    Implémentation de la classe Joueur
    implémentation de Joueur.h et de Joueur.cpp

--- GameManager/Joueur.h
+++ GameManager/Joueur.h
@@ -2,33 +2,40 @@
 // Created by fiset on 2020-02-11.
 //
 
+
 #ifndef PROJET_JOUEUR_H
 #define PROJET_JOUEUR_H
-#define POINT_DEPLACEMENT=10;
-struct coord {
+#include "..\Projectile\Projectile.h"
+#include "..\Coord.h"
+#define POINTS_DEPLACEMENT=10;
+#define VIE_MAX = 100; 
+
+struct Coord{
 	int x;
 	int y;
 };
 class Joueur {
 private:
 	int vie;
-	coord position;
-	int projectileType;
+	Coord position;
+	int typeProjectile;
 	int pointsDeplacement;
 public:
+	Joueur();
 	int getVie();
-	coord getPosition();
-	int getProjectileType();
-	int getPointDeplacement();
+	Coord getPosition();
+	int getTypePrjectile();
+	int getPointsDeplacement();
 	void setVie(int vie);
-	void setPosition(coord Position);
-	void setProjectileType(int type);
+	void setPosition(Coord position);
+	void setTypeProjectile(int type);
 	void setPointsDeplacement(int points);
 
 	//Deplacer le tank(Positif ver la droit et negatif vers la gauche). Retourne s'il avait assez de point de deplacement
 	bool deplacer(int deplacement);
 	//Enleve le nombre de point de degat a la vie. Retourne si le tank est toujours en vie
 	bool endomager(int degat);
+	Projectile tirer(float force, float angle);
 
 };
 
