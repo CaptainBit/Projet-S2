00cd0b61e8262380c0d33f02af75f166967c3567
felixdupont <fxdupont@hotmail.com>
2020-02-13 16:16:25 -0500

    Petit changement deplacement joueur

--- GameManager/Joueur.cpp
+++ GameManager/Joueur.cpp
@@ -53,7 +53,10 @@ void Joueur::setPointsDeplacement(int points)
 //Deplacer le tank(Positif vers la droite et negatif vers la gauche). Retourne s'il avait assez de point de deplacement
 bool Joueur::deplacer(int deplacement)
 {
-
+	if (pointsDeplacement + abs(deplacement) >= 0) {
+		pointsDeplacement += deplacement;
+		position.x += deplacement;
+	}
 }
 
 //Enleve le nombre de point de degat a la vie. Retourne si le tank est toujours en vie. 
