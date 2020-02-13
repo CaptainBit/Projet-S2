85366264144d8f9a68fcac934d6b282a5cb086ed
Isaac Fiset <34320860+CaptainBit@users.noreply.github.com>
2020-02-13 14:37:53 -0500

    Parabole : Ajout fonction selon y

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,6 @@ int main (int argc, char *argv[]) {
     Parabole p  = Parabole(Vec2(0,0), 45,25);
     for(float i = 0; i < 10; i += 0.1){
         Vec2 c = p.displacement(i);
-        std::cout << "x : " << c.x << " y: " << c.y << std::endl;
+        std::cout << "Y : " << c.y << " Y calculated : " << p.getY(c.x) << std::endl;
     }
 }
