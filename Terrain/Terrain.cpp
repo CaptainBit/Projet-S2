2c178994adad1f2d69ea70c88cb831b5e7f3f26e
CaptainBit <fisetisaac@hotmail.com>
2020-02-13 11:06:40 -0500

    ajout de la classe terrain

---
+++ Terrain/Terrain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fiset on 2020-02-13.
+//
+
+#include "Terrain.h"
---
+++ Terrain/Terrain.h
@@ -0,0 +1,16 @@
+//
+// Created by fiset on 2020-02-13.
+//
+
+#ifndef PROJET_TERRAIN_H
+#define PROJET_TERRAIN_H
+
+#include "../Coord.h"
+
+class Terrain {
+public:
+    Terrain(Coord dimension);
+};
+
+
+#endif //PROJET_TERRAIN_H
